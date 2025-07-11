d2758fa516234ec0c5e746ebc8e4d1218438f0f4
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-11 10:24:24 -0300

    feat: add Banner component with styles and organize project structure

---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import styles from "./Banner.module.css"
+
+const Banner = () => {
+  return (
+    <div className={styles.banner}>
+      <img src="assets/banner.png" alt="Organo Banner" />
+    </div>
+  )
+}
+
+export default Banner
