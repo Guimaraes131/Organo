65fc7db7fe773706b8ea2cd5d8d905c534c076b2
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-11 11:44:57 -0300

    feat: add Button to Form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,21 @@
+import Button from "../Button"
 import InputText from "../InputText"
 import SelectInput from "../SelectInput"
+import styles from "./Form.module.css"
 
 const Form = () => {
   return (
-    <div>
+    <div className={styles.form}>
+
       <form>
+        <h1>Fill in the information to create the employee card.</h1>
+
         <InputText label="Name" placeholder="Type your name"/>
         <InputText label="Role" placeholder="Type your role"/>
         <InputText label="Image" placeholder="Image URL"/>
         <SelectInput label="Group"/>
+
+        <Button>Create Card</Button>
       </form>
     </div>
   )
