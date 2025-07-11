ed13389aafe996c738ed189771e22cb076a10d45
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-11 10:50:23 -0300

    feat: add Form component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Banner from "./components/Banner";
+import Form from "./components/Form";
 import InputText from "./components/InputText";
 
 const App = () => {
   return (
     <>
       <Banner/>
+      <Form/>
     </>
   );
 }
